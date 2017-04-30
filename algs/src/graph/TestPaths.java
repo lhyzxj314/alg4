6542cec718e0f99db058e93a51669232032f5809
xiaojun <lhyzxj314@qq.com>
2017-04-30 11:11:08 +0800

    添加深度优先算法及相关应用

---
+++ algs/src/graph/TestPaths.java
@@ -0,0 +1,21 @@
+package graph;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class TestPaths {
+  public static void main(String[] args) {
+    Graph G = new Graph(new In(args[0]));
+    int   s = Integer.parseInt(args[1]);
+    
+    Paths search = new DepthFirstPaths(G, s);
+    for (int v = 0; v < G.V(); v++) {
+      StdOut.print(s + " to " + v + ": ");
+      if (search.hasPathTo(v))
+        for (int w : search.pathTo(v))
+          if (w == s) StdOut.print(w);
+          else        StdOut.print("-" + w);
+      StdOut.println();
+    }
+  }
+}
