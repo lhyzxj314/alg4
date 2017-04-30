7a4f8c5493913c022919e90f5c80e4c3dd76fb05
xiaojun <lhyzxj314@qq.com>
2017-04-30 13:27:50 +0800

    实现无向图的广度优先搜索算法

--- algs/src/graph/TestPaths.java
+++ algs/src/graph/TestPaths.java
@@ -8,7 +8,7 @@ public class TestPaths {
     Graph G = new Graph(new In(args[0]));
     int   s = Integer.parseInt(args[1]);
     
-    Paths search = new DepthFirstPaths(G, s);
+    Paths search = new BreadthFirstPaths(G, s);
     for (int v = 0; v < G.V(); v++) {
       StdOut.print(s + " to " + v + ": ");
       if (search.hasPathTo(v))
